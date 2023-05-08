4e6e51d03e2ed0df500683976ca1a15f7392c73f
NunaHajima <nikenwidyaa1210@gmail.com>
2023-05-08 13:59:11 +0700

    first commit

---
+++ app/Views/layouts/admin/footer.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Project By : Niken Widya Astuti and Shafa Nabila Hada</span>
+                    </div>
+                </div>
+            </footer>
